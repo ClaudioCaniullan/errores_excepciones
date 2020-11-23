9c898ccd669edc1bd7e82828d3c5b22a8fbbab8e
Maguilera85 <drianketing@gmail.com>
2020-11-23 19:47:30 -0300

    archivo ok

--- condominio.py
+++ condominio.py
@@ -72,7 +72,7 @@ class Condominio:
 
 
 class Guardia:
-    # 3 atributos y 4 metodos
+    # 3 atributos y 4 metodos OK
 
     empresa_contratista = 'Tus guardias'
     tipo_contrato = 'Contrato de plata'
@@ -96,7 +96,7 @@ class Guardia:
 
 
 class UnidadHabitacional:
-    # 3 atributos y 4 metodos
+    # 3 atributos y 4 metodos OK
 
     tipo = 'Material sólido'
     cantidad_habitaciones = 6
@@ -117,7 +117,7 @@ class UnidadHabitacional:
 
 
 class CuentaCorriente:
-    # 3 atributos y 4 metodos
+    # 3 atributos y 4 metodos OK
 
     banco = 'Banco Terra'
     tipo = 'Cuenta Corriente'
@@ -140,7 +140,7 @@ class CuentaCorriente:
 
 
 class Terreno:
-    # 6 atributos y 6 metodos
+    # 6 atributos y 6 metodos OK
 
     nombre_dueño_terreno = 'Metropolis'
     seguros = 'terreno con seguros comprometidos'
@@ -160,9 +160,12 @@ class Terreno:
 
     def factibilidad_electricidad(self):
         print('100 por ciento de factibilidad de electricidad')
+        
 
     def factiilidad_servicios_digitales(self):
-        print('100 por ciento de factibilidad de servicios digitales')
+        self.message = '100 por ciento de factibilidad de servicios digitales'
+        self.naci_aqui = 'Soy un mensaje que viene de la clase Terreno'
+        return self.message, self.naci_aqui
 
     def seguridad_terreno(self):
         print('El terreno cuenta con todas las normas de seguridad para construir')
@@ -183,8 +186,9 @@ class Comunidad:
         print('Ven a participar del comite de mascotas')
 
     def JuegosPasatiempos(self):
-        print('Puedes visitar las áreas de esparcimiento')
-
+        self.mensaje = 'Puedes visitar las áreas de esparcimiento'
+        self.naci_aqui2 = 'Este es un mensaje que viene de la clase Comunidad'
+        return self.mensaje
 
 
 # Crear 5 atributos y 6 metodos propios que diferencien a cada una de estas subclases . Demostrar 2 casos de polimorfismo en metodos.
@@ -192,80 +196,108 @@ class CondominioVertical(Terreno, Comunidad):
 
     # 5 atributos
     tipo_condominio_vertical = 'Edificios habitacionales'
+    terreno_km2 = 48524
+    areas_verdes = 1425
+    riego_automatico = True
+    seguridad = True
 
     def __init__(self, nombre_condominio_vertical):
         self.nombre_condominio_vertical = nombre_condominio_vertical
         super().factibilidad_agua_potable()
 
-    # llamamoa a métodos de la clase Terreno
-    
-    #super().factibilidad_electricidad()
-    #super().factiilidad_servicios_digitales()
+    # llamamos a métodos de la clase Terreno
+    def met_terr(self):
+        super().factibilidad_agua_potable()
+        super().factibilidad_electricidad()
+
+    def polimorfeame1(self):
+        super().factiilidad_servicios_digitales()
+        self.message = 'Antes decia lo siguiente: 100 por ciento de factibilidad de electricidad'
+        return self.message, self.naci_aqui
 
     # llamamos a métodos de la clase Comunidad
-    #super().ComiteMascotas()
-    #super().JuegosPasatiempos()
+    def met_com(self):
+        super().ComiteMascotas()
+
+    def polimorfeame2(self):
+        super().JuegosPasatiempos()
+        self.mensaje = 'Antes decia lo siguiente: Puedes visitar las áreas de esparcimiento'
+        return self.mensaje, self.naci_aqui2
 
     # métodos propios de la clase CondominioVertical
     def Ascensor(self):
-        pass
+        print("En estos momentos el servicio de ascensor se encuentra en mantenimiento, disculpe las molestias")
 
     def Terraza(self):
-        pass
+        print("Hermosas terrezas, únicas en Chile")
 
-    def Quincho(sefl):
-        pass
+    def Quincho(self):
+        print("Los mejores asados en tu quincho 3.0 'Lo último en tecnología'")
 
     def Piscina(self):
-        pass
+        print("Descubra las nuevas piscinas sin agua 'Antiahogos'")
 
     def Gym(self):
-        pass
+        print("Gym el Primo. Sin dolor no hay gloria")
 
-
-
-
-
-class CondominioHorizontal(Terreno, Comunidad):
+class CondominioHorizontal(Terreno, Comunidad): # Se asignan 5 atributos
     # 5 atributos
     tipo_condominio_horizontal = 'Conjunto privado de casas'
+    color = "Amarillo"
+    juegos = True
+    balcones = False
+    m2 = 1000
 
-    def __init__(self, nombre_condominio_h):
+    def __init__(self, nombre_condominio_h): # Inicializamos el nombre del condominio
         self.nombre_condominio_h = nombre_condominio_h
-        # llamamoa a métodos de la clase Terreno
-        super().factibilidad_electricidad()
-        super().factiilidad_servicios_digitales(
+
+    # llamamoa a métodos de la clase Terreno
+    def terrenox(self):
         super().factibilidad_agua_potable()
+        super().factibilidad_electricidad()
+        super().factiilidad_servicios_digitales()
 
+    #polimorfismo_1
+    def factibilidad_agua_potable(self):
+        super().factibilidad_agua_potable()
+        self.mensaje = "Ya no tiene agua potable"
+        return self.mensaje
 
+    #polimorfismo_2
 
-)
+    def consulta_hta(self):
+        super().consulta_hta()
+        self.mensaje = "Ahora las hectareas edificales consisten en 20 hectareas"
+        return self.mensaje
 
     # llamamos a métodos de la clase Comunidad
-    #super().ComiteMascotas()
-    #super().JuegosPasatiempos()
+    def condominiox(self):
+        super().ComiteMascotas()
+        super().JuegosPasatiempos()
 
     # métodos propios de la clase CondomioHorizontal
 
-    def Plaza(self):
-        pass
+    def cine(self):
+        print("Contamos con una sala de cina en la junta vecinal")
 
-    def Multicancha(self):
-        pass
+    def multicancha(self):
+        print("Multicanchas de futbol y tenis")
 
-    def Estacionamietos(self):
-        pass
+    def estacionamietos(self):
+        print("Estacionamientos de visita sin limite de tiempo")
 
-    def PuntoVerde(self):
-        pass
+    def ciclovia(self):
+        print("Tiene ciclovia")
 
-    def Minimarket(self):
-        pass
+    def minimarket(self):
+        print("Contamos con nuestros propios MiniMarket's dentro del condominio")
 
 
 
 if __name__ == "__main__":
+    
     # Pruebas de la clase Condominio
+    print("\n***** Aquí comienzan las pruebas en clase Condominio")
     condominio1 = Condominio() # Creo instancia de condominio
     print(condominio1.get_direccion()) # muestra direccion
     condominio1.set_direccion("Esta es la nueva direccion") # cambio direccion
@@ -278,6 +310,25 @@ if __name__ == "__main__":
     print(condominio1.get_guardias()) # Imprimo lista de guardias, para verificacion
     condominio1.del_guardia("Guardia a eliminar") # elimino guarida de lista
     print(condominio1.get_guardias()) # Imprimo lista de guardias, para verificacion
+    
+    # Pruebas en clase CondominioVertical
+    print("\n***** Aquí comienzan las pruebas en clase CondominioVertical")
+    edificio1 = CondominioVertical('Soy el edificio 1')
+    edificio2 = CondominioVertical('Soy el edificio 2')
+    print("Estos son los dos edificios creados:", edificio1.nombre_condominio_vertical, "y", edificio2.nombre_condominio_vertical)
+    print("Aca tenemos un mensaje usando polimorfismo: \n", edificio1.polimorfeame1())
+    print("Este es la segunda prueba de uso de polimorfismo: \n", edificio2.polimorfeame2())
 
+    # Pruebas CondominioHorizontal
+    print("### CONDOMINIO HORIZONTAL ###")
+    condominio_horizonal = CondominioHorizontal("LOS PERALES")
+    condominio_horizonal2 = CondominioHorizontal("GUARDIA VIEJA")
+    print("El nombre del condominio es:", condominio_horizonal.nombre_condominio_h, "&", condominio_horizonal2.nombre_condominio_h) 
+    print("#### APLICANDO POLIMORFISMO ####", condominio_horizonal.factibilidad_agua_potable())
+    print("#### APLICANDO POLIMORFISMO ####", condominio_horizonal.consulta_hta())
+    condominio_horizonal.cine()
+    condominio_horizonal.multicancha()
+    condominio_horizonal.estacionamietos()
+    condominio_horizonal.ciclovia()
+    condominio_horizonal.minimarket()
 
-input()
